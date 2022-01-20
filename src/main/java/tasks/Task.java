65ad47770b11a37ada2a3db188d5aa835344a616
thewrik <wrik@u.nus.edu>
2022-01-20 15:11:34 +0800

    Level-3 Mark as Done
    Mark and Unmark functionality added to each task.

--- src/main/java/tasks/Task.java
+++ src/main/java/tasks/Task.java
@@ -2,12 +2,23 @@ package tasks;
 
 public class Task {
     private String taskName;
+    private Boolean isDone;
 
     public Task(String taskName) {
         this.taskName = taskName;
+        this.isDone = Boolean.FALSE;
+    }
+
+    public void mark() {
+        this.isDone = Boolean.TRUE;
+    }
+
+    public void unmark() {
+        this.isDone = Boolean.FALSE;
     }
 
     public String toString() {
-        return taskName;
+        String isDoneCharacter = isDone ? "X" : " ";
+        return String.format("[%s] %s", isDoneCharacter, taskName);
     }
 }
--- src/main/java/tasks/TaskManager.java
+++ src/main/java/tasks/TaskManager.java
@@ -23,9 +23,29 @@ public class TaskManager {
         return prompt.toString().trim();
     }
 
+    private static String mark(int index) {
+        Task targetTask = taskList.get(index - 1);
+        targetTask.mark();
+        StringBuilder prompt = new StringBuilder("Well done! I have marked this task as done.\n");
+        prompt.append(targetTask.toString());
+        return prompt.toString().trim();
+    }
+
+    private static String unmark(int index) {
+        Task targetTask = taskList.get(index - 1);
+        targetTask.unmark();
+        StringBuilder prompt = new StringBuilder("No worries, I have unmarked this task, good luck!\n");
+        prompt.append(targetTask.toString());
+        return prompt.toString().trim();
+    }
+
     public static void process(String inputMessage) {
         if (inputMessage.equals("list")) {
             Style.printStylised(list());
+        } else if (inputMessage.startsWith("mark")) {
+            Style.printStylised(mark(Integer.valueOf(inputMessage.substring("mark".length() + 1))));
+        } else if (inputMessage.startsWith("unmark")) {
+            Style.printStylised(unmark(Integer.valueOf(inputMessage.substring("unmark".length() + 1))));
         } else {
             Style.printStylised(add(inputMessage));
         }
